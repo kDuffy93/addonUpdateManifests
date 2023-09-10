aa7d453be3e197f0d7529dfc864013d7d92c3f38
Kyle Duffy <kyleduffy83@gmail.com>
2023-09-09 23:31:32 -0400

    football
    adding foodball funtionality and refactoring a few things. also hopefully automatic updates

--- bet356/bet365.js
+++ bet356/bet365.js
@@ -38,11 +38,18 @@ let saySentense = (sentense) => {
 };
 
 let temp = () => {
-    try {
-        let border = document.getElementById("border");
-        let gameName = document.getElementsByClassName("ipe-EventHeader_Fixture ")[0].textContent;
+    let border;
+        let gameName;
         let index;
         let indexLength;
+        let teamName1;
+        let teamName2;
+
+    try {
+         border = document.getElementById("border");
+         gameName = document.getElementsByClassName("ipe-EventHeader_Fixture ")[0].textContent;
+         index;
+         indexLength;
 
         if (gameName.includes('vs')) {
             index = gameName.indexOf(" vs ");
@@ -55,33 +62,94 @@ let temp = () => {
             indexLength = 0;
         }
 
-        let teamName1 = gameName.slice(0, index)
-        let teamName2 = gameName.slice(index + indexLength)
+         teamName1 = gameName.slice(0, index)
+         teamName2 = gameName.slice(index + indexLength)
         console.log(`${teamName1} versus ${teamName2}`);
+    }
+    catch {
+        console.log('ERROR - No Game Loaded')
+    }
 
 
-        let field = document.getElementsByClassName("ml18-CommonAnimations_H2Text ")[0];
+    let field;
+    if (gameName) {
+
+        try {
+            field = document.getElementsByClassName("ml18-CommonAnimations_H2Text ")[0];
+        } catch {
+            console.log('NO basketball fields found')
+        }
 
 
-        
-        let footballField = document.getElementsByClassName("ml12-CommonAnimations_H2Text")[0];
-        let otherfootballField = document.getElementsByClassName("ml12-CommonAnimations_H3Text")[0];
 
-                let parentField;
+        let parentField;
 
         if (!field) {
-            field = document.getElementById('inningsStats-inning').childNodes[0];
-            parentField = document.getElementById('inningsStatsPanel');
+            try {
+                field = document.getElementById('inningsStats-inning').childNodes[0];
+                parentField = document.getElementById('inningsStatsPanel');
+            }
+            catch {
+                console.log('no baseball fields loaded')
+            }
+
         }
 
-        if (field.textContent == "Time Out") {
+        let otherFootballField = {
+            textContent: ''
+        };
 
-            console.log(field.textContent);
+        if (!field) {
+            try {
+                field = document.getElementsByClassName("ml12-CommonAnimations_H2Text ")[0];
+                console.log(field);
+
+            }
+            catch {
+                console.log('no basketball main field found');
+            }
+            try {
+                otherFootballField = document.getElementsByClassName("ml12-CommonAnimations_H3Text ")[0];
+                console.log(otherFootballField);
+            }
+            catch {
+                console.log('no basketball secondary field found');
+            }
+
+        }
+
+
+if(field){
+
+    if (field.textContent == "Time Out") {
+
+        console.log(field.textContent);
+        if (!alreadySpoken) {
+            if (!synth.speaking) {
+                saySentense(`${teamName1}`);
+            }
+        }
+
+
+        if (document.title == `TO - ${teamName1}`) {
+            document.title = `${teamName1}`;
+            border.style.borderColor = 'red';
+        } else {
+            document.title = `TO - ${teamName1}`;
+            border.style.borderColor = 'orange';
+        }
+
+    } else if (field.textContent.includes('Review')) {
+        if (parentField.classList.contains('ml16-BaseballFieldView_Opaque')) {
+
+            
+
             if (!alreadySpoken) {
                 if (!synth.speaking) {
                     saySentense(`${teamName1}`);
                 }
             }
+           
 
 
             if (document.title == `TO - ${teamName1}`) {
@@ -92,67 +160,49 @@ let temp = () => {
                 border.style.borderColor = 'orange';
             }
 
-        } else if (field.textContent.includes('Review')) {
-            if (parentField.classList.contains('ml16-BaseballFieldView_Opaque')) {
+        } else {
 
-               
-
-                if (!alreadySpoken) {
-                    if (!synth.speaking) {
-                        saySentense(`${teamName1}`);
-                    }
-                }
-                
-
-
-                if (document.title == `TO - ${teamName1}`) {
-                    document.title = `${teamName1}`;
-                    border.style.borderColor = 'red';
-                } else {
-                    document.title = `TO - ${teamName1}`;
-                    border.style.borderColor = 'orange';
-                }
-
-            } else {
-
-                border.style.borderColor = "green";
-                document.title = `${teamName1}`;
-                alreadySpoken = false;
-
-            }
-
-        } else if (footballField.textContent.includes('TV Timeout') || otherfootballField.textContent.includes('Timeout')) {
-            console.log("its a football game")
-            console.log(field.textContent);
-            if (!alreadySpoken) {
-                if (!synth.speaking) {
-                    saySentense(`${teamName1}`);
-                }
-            }
-
-
-            if (document.title == `TO - ${teamName1}`) {
-                document.title = `${teamName1}`;
-                border.style.borderColor = 'red';
-            } else {
-                document.title = `TO - ${teamName1}`;
-                border.style.borderColor = 'orange';
-            }
-
-        }
-        else {
-            console.log(field.textContent);
             border.style.borderColor = "green";
             document.title = `${teamName1}`;
             alreadySpoken = false;
 
         }
-    } catch {
-        console.log("No Fields loaded");
+
+    }
+    else if (field.textContent.includes('TV Timeout') || otherFootballField.textContent.includes('Timeout')) {
+
+        if (!alreadySpoken) {
+            if (!synth.speaking) {
+                saySentense(`${teamName1}`);
+            }
+        }
+
+        if (document.title == `TO - ${teamName1}`) {
+            document.title = `${teamName1}`;
+            border.style.borderColor = 'red';
+        } else {
+            document.title = `TO - ${teamName1}`;
+            border.style.borderColor = 'orange';
+        }
+
+
+    }
+
+    else {
+        border.style.borderColor = "green";
+        document.title = `${teamName1}`;
         alreadySpoken = false;
 
+    }
+}
+
+    }// if(game)
+    else{
+        border.style.borderColor = "gray";
+        document.title = `NO GAME FOUND`;
+        alreadySpoken = false;
     }
 };
 
 const intervalID = setInterval(temp, 500);
-console.log('version 5 change - added football compatability');
+console.log('version 6 change to add football functionality');
